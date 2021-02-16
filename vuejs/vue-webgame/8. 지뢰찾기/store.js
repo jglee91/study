437ca66d4042ab90d298dd7c184e47fea73a058e
jglee <dlwnsrb2010@gmail.com>
2021-02-17 01:08:58 +0900

    vuejs tutorial - 8. MineSwiper_2, 9. Router

--- vuejs/vue-webgame/8. 지뢰찾기/store.js
+++ vuejs/vue-webgame/8. 지뢰찾기/store.js
@@ -12,14 +12,14 @@ export const NORMALIZE_CELL = 'NORMALIZE_CELL';
 export const INCREMENT_TIMER = 'INCREMENT_TIMER';
 
 export const CODE = {
-    MINE: -7,
-    NORMAL: -1,
-    QUESTION: -2,
-    FLAG: -3,
-    QUESTION_MINE: -4,
-    FLAG_MINE: -5,
-    CLICKED_MINE: -6,
-    OPENED: 0, // 0 이상이면 다 opened
+  MINE: -7,
+  NORMAL: -1,
+  QUESTION: -2,
+  FLAG: -3,
+  QUESTION_MINE: -4,
+  FLAG_MINE: -5,
+  CLICKED_MINE: -6,
+  OPENED: 0, // 0 이상이면 다 opened
 };
 
 /**
@@ -31,89 +31,187 @@ export const CODE = {
  */
 
 const plantMine = (row, cell, mine) => {
-    console.log(row, cell, mine);
+  console.log(row, cell, mine);
 
-    const candidate = Array(row * cell).fill().map((arr, i) => i);
-    const shuffle = [];
-    while (candidate.length > row * cell - mine) {
-        const chosen = candidate.splice(Math.floor(Math.random() * candidate.length), 1)[0];
-        shuffle.push(chosen);
+  const candidate = Array(row * cell)
+    .fill()
+    .map((arr, i) => i);
+  const shuffle = [];
+  while (candidate.length > row * cell - mine) {
+    const chosen = candidate.splice(
+      Math.floor(Math.random() * candidate.length),
+      1
+    )[0];
+    shuffle.push(chosen);
+  }
+  const data = [];
+  for (let i = 0; i < row; i++) {
+    const rowData = [];
+    data.push(rowData);
+    for (let j = 0; j < cell; j++) {
+      rowData.push(CODE.NORMAL);
     }
-    const data = [];
-    for (let i=0; i<row; i++) {
-        const rowData = [];
-        data.push(rowData);
-        for (let j=0; j<cell; j++) {
-            rowData.push(CODE.NORMAL);
-        }
-    }
+  }
 
-    for (let k=0; k<shuffle.length; k++) {
-        const ver = Math.floor(shuffle[k] / cell);
-        const hor = shuffle[k] % cell;
-        data[ver][hor] = CODE.MINE;
-    }
+  for (let k = 0; k < shuffle.length; k++) {
+    const ver = Math.floor(shuffle[k] / cell);
+    const hor = shuffle[k] % cell;
+    data[ver][hor] = CODE.MINE;
+  }
 
-    console.log(data);
-    return data;
+  console.log(data);
+  return data;
 };
 
 export default new Vuex.Store({
-    state: {
-        tableData: [],
-        data: {
-            row: 0,
-            cell: 0,
-            mine: 0,
-        },
-        timer: 0,
-        halted: true, // 중단된
-        result: '',
+  state: {
+    tableData: [],
+    data: {
+      row: 0,
+      cell: 0,
+      mine: 0,
     },
-    getters: {
-       
+    timer: 0,
+    halted: true, // 중단된
+    openedCount: 0,
+    result: '',
+  },
+  getters: {},
+  mutations: {
+    [START_GAME](state, { row, cell, mine }) {
+      state.data = { row, cell, mine };
+      // 데이터 속성을 일일이 바꿀 경우, Vue.set을 사용할 것
+      // Vue.set(state.data, 'row', row);
+      state.tableData = plantMine(row, cell, mine);
+      state.timer = 0;
+      state.halted = false;
+      state.openedCount = 0;
+      state.result = '';
     },
-    mutations: {
-        [START_GAME](state, { row, cell, mine }) {
-            state.data = { row, cell, mine };
-            // 데이터 속성을 일일이 바꿀 경우, Vue.set을 사용할 것
-            // Vue.set(state.data, 'row', row);
-            state.tableData = plantMine(row, cell, mine);
-            state.timer = 0;
-            state.halted = false;
-        },
-        [OPEN_CELL](state, { row, cell }) {
-            Vue.set(state.tableData[row], cell, CODE.OPENED);
-        },
-        [CLICK_MINE](state) {},
-        [FLAG_CELL](state, { row, cell }) {
-            console.log(state.tableData);
-            console.log(row, cell);
-            if (state.tableData[row][cell] === CODE.MINE) {
-                Vue.set(state.tableData[row], cell, CODE.FLAG_MINE);
-            } else {
-                Vue.set(state.tableData[row], cell, CODE.FLAG);
+    [OPEN_CELL](state, { row, cell }) {
+      let openedCount = 0;
+      const checked = []; // cache algorithm (dynamic programming or memoization)
+      function checkAround(row, cell) {
+        const checkRowOrCellIsUndefined =
+          row < 0 ||
+          row >= state.tableData.length ||
+          cell < 0 ||
+          cell >= state.tableData[0].length;
+        if (checkRowOrCellIsUndefined) {
+          return;
+        }
+        if (
+          [
+            CODE.OPENED,
+            CODE.FLAG,
+            CODE.FLAG_MINE,
+            CODE.QUESTION_MINE,
+            CODE.QUESTION,
+          ].includes(state.tableData[row][cell])
+        ) {
+          return;
+        }
+        // 열었는지 여부 체크
+        if (checked.includes(row + '/' + cell)) {
+          return;
+        } else {
+          checked.push(row + '/' + cell);
+        }
+
+        // 주변 8칸 지뢰인지 검색
+        let around = [];
+        if (state.tableData[row - 1]) {
+          around = around.concat([
+            state.tableData[row - 1][cell - 1],
+            state.tableData[row - 1][cell],
+            state.tableData[row - 1][cell + 1],
+          ]);
+        }
+        around = around.concat([
+          state.tableData[row][cell - 1],
+          state.tableData[row][cell + 1],
+        ]);
+        if (state.tableData[row + 1]) {
+          around = around.concat([
+            state.tableData[row + 1][cell - 1],
+            state.tableData[row + 1][cell],
+            state.tableData[row + 1][cell + 1],
+          ]);
+        }
+        const counted = around.filter(function(v) {
+          return [CODE.MINE, CODE.FLAG_MINE, CODE.QUESTION_MINE].includes(v);
+        });
+
+        if (counted.length === 0 && row > -1) {
+          // 주변칸에 지뢰가 하나도 없으면
+          const near = [];
+          if (row - 1 > -1) {
+            near.push([row - 1, cell - 1]);
+            near.push([row - 1, cell]);
+            near.push([row - 1, cell + 1]);
+          }
+          near.push([row, cell - 1]);
+          near.push([row, cell + 1]);
+          if (row + 1 < state.tableData.length) {
+            near.push([row + 1, cell - 1]);
+            near.push([row + 1, cell]);
+            near.push([row + 1, cell + 1]);
+          }
+          near.forEach((n) => {
+            if (state.tableData[n[0]][n[1]] !== CODE.OPENED) {
+              checkAround(n[0], n[1]);
             }
-        },
-        [QUESTION_CELL](state, { row, cell }) {
-            if (state.tableData[row][cell] === CODE.FLAG_MINE) {
-                Vue.set(state.tableData[row], cell, CODE.QUESTION_MINE);
-            } else {
-                Vue.set(state.tableData[row], cell, CODE.QUESTION);
-            }
-        },
-        [NORMALIZE_CELL](state, { row, cell }) {
-            if (state.tableData[row][cell] === CODE.QUESTION_MINE) {
-                Vue.set(state.tableData[row], cell, CODE.MINE);
-            } else {
-                Vue.set(state.tableData[row], cell, CODE.NORMAL);
-            }
-        },
-        [INCREMENT_TIMER](state) {
-            state.timer += 1;
-        },
+          });
+        }
+        if (state.tableData[row][cell] === CODE.NORMAL) {
+          openedCount += 1;
+        }
+        Vue.set(state.tableData[row], cell, counted.length);
+      }
+      checkAround(row, cell);
+      let halted = false;
+      let result;
+      if (
+        state.data.row * state.data.cell - state.data.mine ===
+        state.openedCount + openedCount
+      ) {
+        halted = true;
+        result = `${state.timer}초만에 승리하셨습니다.`;
+      }
+      state.openedCount += openedCount;
+      state.halted = halted;
+      state.result = result;
     },
-    actions: {
-        
+    [CLICK_MINE](state, { row, cell }) {
+      state.halted = true;
+      Vue.set(state.tableData[row], cell, CODE.CLICKED_MINE);
     },
+    [FLAG_CELL](state, { row, cell }) {
+      console.log(state.tableData);
+      console.log(row, cell);
+      if (state.tableData[row][cell] === CODE.MINE) {
+        Vue.set(state.tableData[row], cell, CODE.FLAG_MINE);
+      } else {
+        Vue.set(state.tableData[row], cell, CODE.FLAG);
+      }
+    },
+    [QUESTION_CELL](state, { row, cell }) {
+      if (state.tableData[row][cell] === CODE.FLAG_MINE) {
+        Vue.set(state.tableData[row], cell, CODE.QUESTION_MINE);
+      } else {
+        Vue.set(state.tableData[row], cell, CODE.QUESTION);
+      }
+    },
+    [NORMALIZE_CELL](state, { row, cell }) {
+      if (state.tableData[row][cell] === CODE.QUESTION_MINE) {
+        Vue.set(state.tableData[row], cell, CODE.MINE);
+      } else {
+        Vue.set(state.tableData[row], cell, CODE.NORMAL);
+      }
+    },
+    [INCREMENT_TIMER](state) {
+      state.timer += 1;
+    },
+  },
+  actions: {},
 });
---
+++ vuejs/vue-webgame/9. vue-router/main.js
@@ -0,0 +1,6 @@
+import Vue from 'vue';
+
+import Router from './Router';
+
+// vue instance
+new Vue(Router).$mount('#root');
