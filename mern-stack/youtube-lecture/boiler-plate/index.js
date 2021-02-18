d2ae49b71a278185157ad63ae01415b608f723b0
jglee <dlwnsrb2010@gmail.com>
2021-02-19 00:28:16 +0900

    mern stack - youtube, boiler plate ( ~ ch09)

---
+++ mern-stack/youtube-lecture/boiler-plate/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+require('dotenv').config();
+
+const { User } = require('./model/user');
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('DB connected'))
+  .catch((err) => console.error(err));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+app.post('/api/users/register', (req, res) => {
+  const user = new User(req.body);
+
+  user.save((err, userData) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({ success: true });
+  });
+});
+
+app.listen(5000);
